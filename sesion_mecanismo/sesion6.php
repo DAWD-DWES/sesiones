4cbfee8a056c69285f59ba6c6e41382930431804
jivanrod <jivanrod@gmail.com>
2025-01-10 16:15:17 +0100

    Update sesion6.php

--- sesion_mecanismo/sesion6.php
+++ sesion_mecanismo/sesion6.php
@@ -6,6 +6,6 @@ session_start();
 // loop through the session array with foreach
 foreach ($_SESSION['animals'] as $key => $value) {
     // and print out the values
-    echo 'The value of $_SESSION[' . "'" . $key . "'" . '] is ' . "'" . $value . "'" . ' <br />';
+    echo 'The value of $_SESSION["animals"][' . "'" . $key . "'" . '] is ' . "'" . $value . "'" . ' <br />';
 }
 ?>
