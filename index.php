048a4bd7c2e6df4a105e55793f83a59ec0784a9e
jivanrod <jivanrod@gmail.com>
2025-01-16 17:00:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,25 +8,20 @@ if (isset($_SESSION['usuario'])) {
         session_destroy();
         $params = session_get_cookie_params();
         setcookie(session_name(), '', time() - 42000,
-            $params["path"], $params["domain"],
-            $params["secure"], $params["httponly"]
-    );
-        $loggedIn = false;
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]);
     } else {
         $loggedIn = true;
     }
 } else {
-    if (empty($_POST)) {
-        $loggedIn = false;
-    } elseif (filter_input(INPUT_POST, 'botonenviologin')) {
+    if (filter_input(INPUT_POST, 'botonenviologin')) {
         $nombre = trim(filter_input(INPUT_POST, 'nombre', FILTER_UNSAFE_RAW));
         $contrasenia = trim(filter_input(INPUT_POST, 'contrasenia', FILTER_UNSAFE_RAW));
-        if ($nombre === 'ivan' && $contrasenia === 'ivan') {
+        if ($nombre == 'ivan' && $contrasenia == 'ivan') {
             $_SESSION['usuario'] = $nombre;
             $loggedIn = true;
         } else {
             $invalidAccess = true;
-            $loggedIn = false;
         }
     }
 }
@@ -40,21 +35,21 @@ if (isset($_SESSION['usuario'])) {
         <link rel="stylesheet" href="stylesheet.css">
     </head>
     <body>
-        <?php if (!$loggedIn): ?>
+        <?php if (!($loggedIn ?? false)): ?>
             <h1>Formulario de Login de Cliente</h1>
-            <div class = "capaform">
-                <form class = "form-font" name = "Formlogin"
-                      action = "index.php" method = "POST">
-                    <div class = "form-section">
-                        <label for = "nombre">Nombre:</label>
-                        <input id = "nombre" type = "text" required = "required" name = "nombre" size = "30" >
+            <div class="capaform">
+                <form class="form-font" name="Formlogin"
+                      action="index.php" method="POST">
+                    <div class="form-section">
+                        <label for="nombre">Nombre:</label>
+                        <input id="nombre" type="text" required="required" name="nombre" size="30" >
                     </div>
-                    <div class = "form-section">
-                        <label for = "contrasenia">Contraseña:</label>
-                        <input id = "contrasenia" type = "password" required = "required" name = "contrasenia" size = "20" >
+                    <div class="form-section">
+                        <label for="contrasenia">Contraseña:</label>
+                        <input id="contrasenia" type="password" required="required" name="contrasenia" size="20" >
                     </div>
-                    <input class = "submit" type = "submit"
-                           value = "Enviar" name = "botonenviologin" >
+                    <input class="submit" type="submit"
+                           value="Enviar" name="botonenviologin" >
                 </form>
                 <?php if ($invalidAccess ?? false): ?>
                     <h1><?= ERROR_MESSAGE ?></h1>
